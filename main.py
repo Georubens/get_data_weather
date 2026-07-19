954ba66de27c82c1979b2f492a8f504176c588a5
Georubens <geovannyrubens@gmail.com>
2026-07-19 23:16:48 +0700

    Update v0.3 19072026 23.15

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import pandas as pd
 import requests
 import os
 import json
+import logging
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
+
+#Setup Logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 #Load environmet from .env
 load_dotenv()
@@ -16,11 +23,31 @@ db_config = {
     'database' : os.getenv('database')
 }
 
-def get_data():
-    url_api = requests.get("https://api.open-meteo.com/v1/forecast?latitude=-7.8014&longitude=110.3647&hourly=temperature_2m,rain,weather_code,precipitation_probability&timezone=Asia%2FBangkok&forecast_days=1")
-    data_json = url_api.json()
-    return data_json
+#Main URL
+url = "https://api.open-meteo.com/v1/forecast?latitude=-7.8014&longitude=110.3647&hourly=temperature_2m,rain,weather_code,precipitation_probability&timezone=Asia%2FBangkok&forecast_days=1"
 
+logging.info("Script Started")
+
+def url_response(url):
+    try:
+        get_url_response = requests.get(url)
+        if get_url_response.status_code == 200:
+            logging.info(f"Response Success : {get_url_response.status_code}")
+        else:
+            logging.info(f"Response Failed : {get_url_response.status_code}")
+            return None
+    except Exception as e:
+        logging.error(f"An Error Occured : {e}")
+
+def get_data(url):
+    try:
+        url_api = requests.get(url)
+        data_json = url_api.json()
+        return data_json
+    except Exception as e:
+        logging.error(f"An Error Occured : {e}")
+        return None
+    
 def main_data(file):
     df = pd.DataFrame(file['hourly'])
     df['city'] = "Yogyakarta"
@@ -35,7 +62,10 @@ def connect_db(file):
     return file
 
 try:
-    main_file = get_data()
+    check_response = url_response(url)
+    main_file = get_data(url)
     cleaned_file = main_data(main_file)
     connect_db(cleaned_file)
-    print("Import Sucess")
+    logging.info("Import Sucess")
+except Exception as e:
+    logging.error(f"Something Went Wrong : {e}")
